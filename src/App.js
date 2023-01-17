88aeb23213d3b026a1554c345c9e6f9f87d2d903
Luis-Valle <al19020414@itsa.edu.mx>
2023-01-16 21:31:24 -0600

    suvir prueva

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <span>Luis Alberto Valle Espinoza</span>
         </a>
       </header>
     </div>
